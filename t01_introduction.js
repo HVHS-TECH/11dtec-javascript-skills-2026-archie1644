13d3aa7163a81e02233b59b3f5199f5677309ee2
archie1644 <24220ac@hvhs.school.nz>
2026-04-23 02:58:40 +0000

    Apr 23, 2026, 2:58 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,10 +1,19 @@
 /****************************
-tung tung sahur comment
-this is my comment
+Introduction
 ****************************/
 console.log("Running t01_introduction.js")
 console.log("skibdi toilet skibidi toilet 67 67 tung tung tung sahur")
 
+//variables
 
-//useful comment
+
+/****************************
+main code
+****************************/
+
+
+
+/****************************
+functions
+****************************/
 
