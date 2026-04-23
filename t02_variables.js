a6613e7f6988fa0ce494d6ba1647baa1e7f64f66
archie1644 <24220ac@hvhs.school.nz>
2026-04-23 02:54:48 +0000

    Apr 23, 2026, 2:54 AM

--- t02_variables.js
+++ t02_variables.js
@@ -2,9 +2,15 @@
 tung tung sahur comment
 this is my comment
 ****************************/
+
+let name = archie;
+let pocketMoney = 2;
+let age = 15;
+let year = 2026;
+
 console.log("Running t01_introduction.js")
 console.log("skibdi toilet skibidi toilet 67 67 tung tung tung sahur")
-
+console.log("Hi "+name)
 
 //useful comment
 
