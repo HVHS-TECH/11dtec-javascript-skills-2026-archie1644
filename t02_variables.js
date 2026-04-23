a2d53f73c26f9110884b44a8691f5bb3749482d5
archie1644 <24220ac@hvhs.school.nz>
2026-04-23 03:14:33 +0000

    lesson 3

--- t02_variables.js
+++ t02_variables.js
@@ -6,18 +6,19 @@ console.log("Running t02_variables.js")
 //variables
 let name = "archie";
 let pocketMoney = 2;
-let age = 15;
+let age = 16;
 let year = 2026;
-
 
 /****************************
 main code
 ****************************/
 console.log("Hi "+ name + ",")
 console.log("as of " + year + ", " + "you are " + age + " years old" + ".")
+console.log("you were born in", year-age)
+console.log("In 10 years, you will be", age+10)
 console.log("you have " + pocketMoney + " " + "dollars.")
-console.log("you were born in " + year - age)
-
+console.log("you spend half of your money, now you have", pocketMoney/2, "dollars")
+console.log("then you get $3, you now have", )
 /****************************
 functions
 ****************************/
