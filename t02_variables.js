7e4fa27fa262b5ff30d0b20e745227bba48b9619
archie1644 <24220ac@hvhs.school.nz>
2026-04-23 02:51:49 +0000

    Apr 23, 2026, 2:51 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,10 @@
-/* Paste your code from task 1 here */
+/****************************
+tung tung sahur comment
+this is my comment
+****************************/
+console.log("Running t01_introduction.js")
+console.log("skibdi toilet skibidi toilet 67 67 tung tung tung sahur")
+
+
+//useful comment
+
