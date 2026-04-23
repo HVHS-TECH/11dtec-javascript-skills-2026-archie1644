6359f28b9a5d9ea3bb9dcba02b0e8b05ac1239d0
archie1644 <24220ac@hvhs.school.nz>
2026-04-23 02:39:47 +0000

    Apr 23, 2026, 2:39 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,9 +1,9 @@
 /****************************
-this is my really cool cool cool
+67 skibdi toilet 67
 tung tung sahur comment
 ****************************/
 console.log("Running t01_introduction.js")
-console.log(/*Insert your message here!*/)
+console.log(/*67*/)
 
 
 //triple t was here 67 skibidi toilet baka
