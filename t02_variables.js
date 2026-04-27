efb1d322febf3faa44eb3dd2a28da6ba1f8b5e3e
archie1644 <24220ac@hvhs.school.nz>
2026-04-27 22:18:50 +0000

    Apr 27, 2026, 10:18 PM

--- t02_variables.js
+++ t02_variables.js
@@ -14,11 +14,7 @@ main code
 ****************************/
 console.log("Hi "+ name + ",")
 console.log("as of " + year + ", " + "you are " + age + " years old" + ".")
-console.log("you were born in", year-age)
-console.log("In 10 years, you will be", age+10)
 console.log("you have " + pocketMoney + " " + "dollars.")
-console.log("you spend half of your money, now you have", pocketMoney/2, "dollars")
-console.log("then you get $3, you now have", )
 /****************************
 functions
 ****************************/
