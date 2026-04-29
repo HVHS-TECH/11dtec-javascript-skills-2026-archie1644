f371258c75844fd0d75af219900a122aa7f6f8d5
archie1644 <24220ac@hvhs.school.nz>
2026-04-29 00:58:30 +0000

    Apr 29, 2026, 12:58 AM

--- t03_maths.js
+++ t03_maths.js
@@ -15,10 +15,14 @@ main code
 console.log("Hi "+ name + ",")
 console.log("as of " + year + ", " + "you are " + age + " years old" + ".")
 console.log("you were born in", year-age)
+var yearBorn = year-age;
 console.log("In 10 years, you will be", age+10)
+var tenYears = age+10;
 console.log("you have " + pocketMoney + " " + "dollars.")
 console.log("you spend half of your money, now you have", pocketMoney/2, "dollars")
+var halfMoney = pocketMoney/2;
 console.log("then you get $3, you now have", pocketMoney/2+3, "dollars" )
+var plusThree = pocketMoney/2+3;
 
 /****************************
 functions
