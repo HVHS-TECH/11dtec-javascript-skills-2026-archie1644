f72d45d8b706381c01ef256758ea0e20f2363d8f
archie1644 <24220ac@hvhs.school.nz>
2026-04-29 01:29:41 +0000

    Apr 29, 2026, 1:29 AM

--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,36 @@
-/* Paste your code from task 5 here */
+/****************************
+Functions
+****************************/
+console.log("Running t06_functions.js")
+
+const OUTPUT = document.getElementById("SpaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>added by javascrript</h2>";
+
+//variables
+let name = "archie";
+let pocketMoney = 2;
+let age = 16;
+let year = 2026;
+var yearBorn = year-age;
+var tenYears = age+10;
+var halfMoney = pocketMoney/2;
+var plusThree = pocketMoney/2+3;
+/****************************
+main code
+****************************/
+OUTPUT.innerHTML += "<p>Hi "+ name;
+OUTPUT.innerHTML += "<p>as of " + year + " you are " + age + " years old";
+OUTPUT.innerHTML += "<p>you were born in " + yearBorn;
+OUTPUT.innerHTML += "<p>In 10 years, you will be " + tenYears;
+OUTPUT.innerHTML += "<p>you have " + pocketMoney + " dollars.";
+OUTPUT.innerHTML += "<p>you spend half of your money, now you have " + halfMoney + " dollars";
+OUTPUT.innerHTML += "<p>then you get $3, you now have " + plusThree + " dollars" ;
+
+/****************************
+functions
+****************************/
+function welcome(){
+    OUTPUT.innerHTML += "<p>welcome to the shop";
+}
+
+welcome()
