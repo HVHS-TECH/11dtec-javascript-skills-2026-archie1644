2d33eca9215fe0b3d32b084b541b19699340173f
archie1644 <24220ac@hvhs.school.nz>
2026-04-29 01:19:11 +0000

    Apr 29, 2026, 1:19 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -15,26 +15,18 @@ let year = 2026;
 /****************************
 main code
 ****************************/
-OUTPUT.innerHTML += <p>"Hi "+ name + ","</p>;
-console.log("as of " + year + "you are " + age + " years old")
-console.log("you were born in", year-age)
+OUTPUT.innerHTML += "<p>Hi "+ name;
+OUTPUT.innerHTML += "<p>as of " + year + " you are " + age + " years old";
+OUTPUT.innerHTML += "<p>you were born in" + year - age;
 var yearBorn = year-age;
-console.log("In 10 years, you will be", age+10)
+OUTPUT.innerHTML += "<p>In 10 years, you will be", age+10;
 var tenYears = age+10;
-console.log("you have " + pocketMoney + " dollars.")
-console.log("you spend half of your money, now you have", pocketMoney/2, "dollars")
+OUTPUT.innerHTML += "<p>you have " + pocketMoney + " dollars.";
+OUTPUT.innerHTML += "<p>you spend half of your money, now you have", pocketMoney/2, "dollars";
 var halfMoney = pocketMoney/2;
-console.log("then you get $3, you now have", pocketMoney/2+3, "dollars" )
+OUTPUT.innerHTML += "<p>then you get $3, you now have", pocketMoney/2+3, "dollars" ;
 var plusThree = pocketMoney/2+3;
 
-
-OUTPUT.innerHTML += "<p>Hi " + name + ",</p>"
-OUTPUT.innerHTML += "<p>as of " + year + ", " + "you are " + age + " years old" + ".</p>"
-OUTPUT.innerHTML += "<p>you were born in " + yearBorn + ".</p>"
-OUTPUT.innerHTML += "<p>In 10 years, you will be " + age+10 + ".</p>"
-OUTPUT.innerHTML += "<p>you have " + pocketMoney + " dollars</p>"
-OUTPUT.innerHTML += "<p>you spend half of your money, now you have " + pocketMoney/2 + " dollar.</p>"
-OUTPUT.innerHTML += "<p>then you get $3, you now have " + pocketMoney/2+3 + " dollars";
 
 /****************************
 functions
