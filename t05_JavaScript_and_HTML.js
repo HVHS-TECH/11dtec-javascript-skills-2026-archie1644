b0c744c721c077a9bbb042372c089aaf75c5d515
archie1644 <24220ac@hvhs.school.nz>
2026-04-29 00:48:35 +0000

    Apr 29, 2026, 12:48 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -15,13 +15,13 @@ let year = 2026;
 /****************************
 main code
 ****************************/
-OUTPUT.innerHTML += <p>"Hello " + name + ","</p>
-OUTPUT.innerHTML += "as of " + year + ", " + "you are " + age + " years old"
-OUTPUT.innerHTML += "you were born in, " + year-age
-OUTPUT.innerHTML += "In 10 years, you will be, " + age+10
-OUTPUT.innerHTML += "you have " + pocketMoney + " " + "dollars."
-OUTPUT.innerHTML += "you spend half of your money, now you have " + pocketMoney/2 + " dollars"
-OUTPUT.innerHTML += "then you get $3, you now have " + pocketMoney/2 + " dollars" 
+OUTPUT.innerHTML += "<p>Hello " + name + ","
+OUTPUT.innerHTML += "<p>as of " + year + ", " + "you are " + age + " years old"
+OUTPUT.innerHTML += "<p>you were born in, " + year-age + "."
+OUTPUT.innerHTML += "<p>In 10 years, you will be, " + age+10
+OUTPUT.innerHTML += "<p>you have " + pocketMoney + " " + "dollars."
+OUTPUT.innerHTML += "<p>you spend half of your money, now you have " + pocketMoney/2 + " dollars"
+OUTPUT.innerHTML += "<p>then you get $3, you now have " + pocketMoney/2 + " dollars" 
 
 /****************************
 functions
