964e3506543e8eb6183c310af55d34cc0474dfdb
archie1644 <24220ac@hvhs.school.nz>
2026-04-29 00:51:13 +0000

    Apr 29, 2026, 12:51 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -15,7 +15,7 @@ let year = 2026;
 /****************************
 main code
 ****************************/
-OUTPUT.innerHTML += ("Hi "+ name + ",")
+OUTPUT.innerHTML +=("Hi "+ name + ",")
 console.log("as of " + year + ", " + "you are " + age + " years old" + ".")
 console.log("you were born in", year-age)
 console.log("In 10 years, you will be", age+10)
