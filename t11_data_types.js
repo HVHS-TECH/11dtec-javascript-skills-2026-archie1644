55a2b8c8e1eeb9ee15005be5c082bf5fae7bc360
archie1644 <24220ac@hvhs.school.nz>
2026-05-07 02:20:03 +0000

    May 7, 2026, 2:20 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,1 +1,62 @@
-/* Paste your code from the last task */
+/****************************
+data types
+****************************/
+
+console.log("Running t11_data_types.js")
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML += "<h2>added by javascrript</h2>";
+
+//variables
+let name = "archie";
+let pocketMoney = 2;
+let age = 16;
+let year = 2026;
+var yearBorn = year-age;
+var tenYears = age+10;
+var halfMoney = pocketMoney/2;
+var plusThree = pocketMoney/2+3;
+/****************************
+main code
+****************************/
+function start(){
+OUTPUT.innerHTML += "<p>Hi "+ name;
+OUTPUT.innerHTML += "<p>as of " + year + " you are " + age + " years old";
+OUTPUT.innerHTML += "<p>you were born in " + yearBorn;
+OUTPUT.innerHTML += "<p>In 10 years, you will be " + tenYears;
+OUTPUT.innerHTML += "<p>you have " + pocketMoney + " dollars.";
+OUTPUT.innerHTML += "<p>you spend half of your money, now you have " + halfMoney + " dollars";
+OUTPUT.innerHTML += "<p>then you get $3, you now have " + plusThree + " dollars" ;
+welcome()
+displayProduct("<p>chocolate: ", "$4.00");
+displayProduct("<p>chips: ", "$3.00");
+displayProduct("<p>drink: ", "$2.50");
+}
+
+/****************************
+functions
+****************************/
+
+function getFormInput(){
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
+}
+
+function getFormInput(){
+            const AGE_FIELD = document.getElementById("ageField");
+    let userAge = NUMBER(AGE_FIELD.value);
+    OUTPUT.innerHTML = "<p>Your age is "+userAge+"</p>";
+}
+
+
+
+function welcome(){
+    OUTPUT.innerHTML += "<p>welcome to the shop:";
+}
+
+function displayProduct(_name, _price){
+    OUTPUT.innerHTML += _name + _price
+}
+
+
+
