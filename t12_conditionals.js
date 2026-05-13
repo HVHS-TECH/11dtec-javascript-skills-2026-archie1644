41d4e1244b96ff7f5ca39e8591722723907d1276
archie1644 <24220ac@hvhs.school.nz>
2026-05-13 00:52:25 +0000

    May 13, 2026, 12:52 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -25,21 +25,28 @@ let year = 2026;
 var yearBorn = year-Userage;
 var tenYears = Userage+10;
 var halfMoney = UserpocketMoney/2;
-var plusThree = UserpocketMoney/2+3;
+var pocketMoney2 = UserpocketMoney/2+3;
 OUTPUT.innerHTML += "<p>Hi "+ Username;
 OUTPUT.innerHTML += "<p>as of " + year + " you are " + Userage + " years old";
 OUTPUT.innerHTML += "<p>you were born in " + yearBorn;
 OUTPUT.innerHTML += "<p>In 10 years, you will be " + tenYears;
 OUTPUT.innerHTML += "<p>you have " + UserpocketMoney + " dollars.";
 OUTPUT.innerHTML += "<p>you spend half of your money, now you have " + halfMoney + " dollars";
-OUTPUT.innerHTML += "<p>then you get $3, you now have " + plusThree + " dollars" ;
+OUTPUT.innerHTML += "<p>then you get $3, you now have " + pocketMoney2 + " dollars" ;
+
 welcome()
 displayProduct("<p>chocolate: ", "$4.00");
 displayProduct("<p>chips: ", "$3.00");
 displayProduct("<p>drink: ", "$2.50");
+    if (pocketMoney2 >= 4){
+        OUTPUT.innerHTML += "<p>you have enough to buy a chocolate bar";
+    }
+    else{
+        OUTPUT.innerHTML += "<p>you do not have enough to buy a chocolate bar"
+    }
 
 }
-chocolatebar()
+
 /****************************
 functions
 ****************************/
@@ -51,12 +58,4 @@ function displayProduct(_name, _price){
     OUTPUT.innerHTML += _name + _price
 }
 
-function chocolatebar(){
-if (UserpocketMoney > 4){
-    OUTPUT.innerHTML += "<p>you have enough to buy a chocolate bar"
-} else if (UserpocketMoney == 4){
-    OUTPUT.innerHTML += "<p>you have enough to buy a chocolate bar"
-} else {
-    OUTPUT.innerHTML += "<p>you do not have enough to buy a chocolate bar"
-}
-}
+
